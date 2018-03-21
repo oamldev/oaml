ab8cc97377c92fd4862fa24c6274d646d8fe0c88
Marcelo Fernandez <marcelofg55@gmail.com>
2018-03-20 21:12:31 -0300

    Updated copyright line

--- src/test.cpp
+++ src/test.cpp
@@ -1,5 +1,5 @@
 //-----------------------------------------------------------------------------
-// Copyright (c) 2015-2017 Marcelo Fernandez
+// Copyright (c) 2015-2018 Marcelo Fernandez
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to
